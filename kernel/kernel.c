c4240d7378335479c71f1858c9bedfa73a4f54ed
DinoMalin <juliancario@orange.fr>
2024-09-24 23:37:12 +0200

    + | Add functions to write in the console

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,59 @@
+# define LIN 24
+# define COL 80
+
+# define BLACK      0
+# define BLUE       1
+# define GREEN      2
+# define CYAN       3
+# define RED        4
+# define MAGENTA    5
+# define BROWN      6
+# define LGRAY      7
+
+# define DGRAY      8
+# define LBLUE      9
+# define LGREEN     10
+# define LCYAN      11
+# define LRED       12
+# define LMAGENTA   13
+# define YELLOW     14
+# define WHITE      15
+
+# define combine(bg, fg) (bg * 16 + fg)
+
+unsigned char *video_memory = (unsigned char *)0xB8000;
+
+void putshark(unsigned char c, unsigned char color, int x, int y)
+{
+    if (x > COL)
+        x = 0;
+    if (y > LIN)
+        y = LIN;
+
+    int offset = (y * LIN + x) * 2;
+    video_memory[offset] = c;
+    video_memory[offset + 1] = color;
+}
+
+void putstrk(char *str, unsigned char color, int x, int y)
+{
+    for (int i = 0; str[i]; i++)
+    {
+        if (x > COL)
+        {
+            x = 0;
+            y++;
+        }
+        if (y > LIN)
+        {
+            y = LIN;
+            x = 0;
+        }
+        putshark(str[i], color, x++, y);
+    }
+}
+
 void kmain()
 {
-    unsigned char *video_memory = (unsigned char *)0xB8000;
-
-    video_memory[0] = '4';
-    video_memory[1] = 0xF1;
-    video_memory[2] = '2';
-    video_memory[3] = 0xF1;
-    video_memory[4] = 'D';
-    video_memory[5] = 0x1F;
-    video_memory[6] = 'I';
-    video_memory[7] = 0x1F;
-    video_memory[8] = 'N';
-    video_memory[9] = 0x1F;
-    video_memory[10] = 'O';
-    video_memory[11] = 0x1F;
+    putstrk("42 | DinoMalin was here", combine(WHITE, BLUE), 0, 0);
 }
