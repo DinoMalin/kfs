62003af967f5c02c49644b4471f32320a7dddf42
DinoMalin <juliancario@orange.fr>
2024-09-28 17:12:19 +0200

    ~ | Using macros for gdt

--- utils/gdt.c
+++ utils/gdt.c
@@ -1,56 +1,36 @@
 #include "dinoint.h"
 #include "gdt.h"
 
-struct gdt_entry
-{
-    unsigned short limit_low __attribute__((packed));
-    unsigned short base_low __attribute__((packed));
-    unsigned char base_middle __attribute__((packed));
-    unsigned char access __attribute__((packed));
-    unsigned char granularity __attribute__((packed));
-    unsigned char base_high __attribute__((packed));
-} __attribute__((packed));
-
-struct gdt_ptr
-{
-    unsigned short limit __attribute__((packed));
-    unsigned int base __attribute__((packed));
-} __attribute__((packed));
-
-struct gdt_entry gdt[3];
+struct gdt_entry gdt[NB_SEGMENTS];
 struct gdt_ptr gp;
 
 extern void gdt_flush();
 
-// Setup a descriptor in the Global Descriptor Table
-void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) {
-    // Setup the descriptor base address
-    gdt[num].base_low = (base & 0xFFFF);
-    gdt[num].base_middle = (base >> 16) & 0xFF;
-    gdt[num].base_high = (base >> 24) & 0xFF;
+void gdt_set_gate(unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) {
+    static int nb = 0;
 
-    // Setup the descriptor limits
-    gdt[num].limit_low = (limit & 0xFFFF);
-    gdt[num].granularity = ((limit >> 16) & 0x0F);
+    gdt[nb].base_low = (base & 0xFFFF);
+    gdt[nb].base_middle = (base >> 16) & 0xFF;
+    gdt[nb].base_high = (base >> 24) & 0xFF;
 
-    // Finally, set up the granularity and access flags
-    gdt[num].granularity |= (gran & 0xF0);
-    gdt[num].access = access;
+    gdt[nb].limit_low = (limit & 0xFFFF);
+
+    gdt[nb].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
+    gdt[nb].access = access;
+
+    nb++;
 }
 
 void gdt_install() {
-    // Setup the GDT pointer and limit
-    gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
+    gp.limit = (sizeof(struct gdt_entry) * NB_SEGMENTS) - 1;
     gp.base = &gdt;
 
-    // Null Descriptor
-    gdt_set_gate(0, 0, 0, 0, 0);
-    // Code segment
-    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
-    // Data segment
-    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+    gdt_set_gate(null_descriptor);
+    gdt_set_gate(kernel_code);
+    gdt_set_gate(kernel_data);
+    gdt_set_gate(user_code);
+    gdt_set_gate(user_data);
 
-    // Flush out the old GDT and install new changes
     gdt_flush();
 }
 
--- utils/gdt.h
+++ utils/gdt.h
@@ -1,4 +1,43 @@
 #pragma once
 
-void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
+# define packed __attribute__((packed))
+
+struct gdt_entry
+{
+    unsigned short limit_low packed;
+    unsigned short base_low packed;
+    unsigned char base_middle packed;
+    unsigned char access packed;
+    unsigned char granularity packed;
+    unsigned char base_high packed;
+} packed;
+
+struct gdt_ptr
+{
+    unsigned short limit packed;
+    unsigned int base packed;
+} packed;
+
 void gdt_install();
+
+ /* In 32 bits, we can use at max 4Go of RAM.
+  *	We don't really want to use segmentation,
+  *	as it is not really practical.
+  *	What we do is giving every segment the
+  *	whole RAM, letting them overlap
+  *	themselves, so we use segmentation without
+  *	using it.
+  * */
+# define LIMIT_MAX 0xFFFFFFFF
+
+/* See https://wiki.osdev.org/Global_Descriptor_Table#Segment_Descriptor
+ * for more informations on access and granularity bytes.
+*/
+
+# define NB_SEGMENTS		5
+# define null_descriptor	0, 0, 0, 0
+# define kernel_code		0, LIMIT_MAX, 0b10011011, 0b11001111
+# define kernel_data		0, LIMIT_MAX, 0b10010011, 0b11001111
+# define user_code		0, LIMIT_MAX, 0b11111011, 0b11001111
+# define user_data		0, LIMIT_MAX, 0b11110011, 0b11001111
+
